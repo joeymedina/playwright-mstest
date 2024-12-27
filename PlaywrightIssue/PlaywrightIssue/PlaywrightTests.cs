f3c880fff518090e89385959ebe5c135daeedeb8
Joseph Medina <joseph.medina@agvantis.com>
2024-12-27 17:34:24 -0600

    initial commit

---
+++ PlaywrightIssue/PlaywrightIssue/PlaywrightTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Playwright.MSTest;
+using Microsoft.Playwright;
+using System.Text.RegularExpressions;
+
+namespace PlaywrightIssue
+{
+    [TestClass]
+    public sealed class PlaywrightBrowserStackSampleTests : PageTest
+    {
+        [TestMethod]
+        public async Task HasTitle()
+        {
+            await Page.GotoAsync("https://playwright.dev");
+
+            await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+        }
+
+        [TestMethod]
+        public async Task GetStartedLink()
+        {
+            await Page.GotoAsync("https://playwright.dev");
+
+            await Page.GetByRole(AriaRole.Link, new() { Name = "Get started" }).ClickAsync();
+
+            await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Installation" })).ToBeVisibleAsync();
+        }
+
+    }
+}
